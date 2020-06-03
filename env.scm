6ca6f1333e9cddc462614a68d84671e6cfecb263
Jacob First <jacob.first@member.fsf.org>
2020-06-03 03:07:45 -0400

    compiling

---
+++ env.scm
@@ -0,0 +1,14 @@
+(specifications->manifest
+ '("node"
+   "rubber"
+   "texlive-base"
+   "texlive-latex-geometry"
+   "texlive-fonts-ec"
+   "texlive-latex-babel"
+   "texlive-latex-pgf"
+   "texlive-latex-ms"
+   "texlive-url"
+
+   ;; "texlive-latex-type1cm"
+   "texlive-fonts-latex"
+   ))
